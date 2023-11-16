51752220b98130aa8efafce361b9b068e9f89865
Chiko but no H <franciscotrianto@gmail.com>
2023-11-16 11:43:16 +0700

    feat: redesign How to Page

--- src/client/src/app/how_to_use/page.tsx
+++ src/client/src/app/how_to_use/page.tsx
@@ -9,73 +9,33 @@ export default function HowToUse() {
           How To
         </h1>
         <h2 className="text-xl font-bold m-3">Very Simple</h2>
-        <div className="flex flex-row text-base m-4 text-white justify-between w-[80%]">
-          <div className="w-[15rem] p-4 bg-[--trinary] rounded-md">
+        <div className="flex flex-row text-base m-4 text-white justify-between w-[80%] text-center">
+          <div className="flex flex-col w-[17rem] p-4 bg-[--trinary] rounded-md items-center">
+            <span className="border-white border-2 text-3xl rounded-full px-4 m-2 text-center font-bold">
+              1
+            </span>
             <h1 className="text-xl font-bold">
               Select and Upload your dataset
             </h1>
-            <p></p>
           </div>
-          <div className="flex justify-center items-center">
-            <svg
-              className="w-12 h-12 text-black"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </div>
-          <div className="w-[15rem] p-4 bg-[--trinary] rounded-md">
+          <div className="flex flex-col w-[17rem] p-4 bg-[--trinary] rounded-md items-center">
+            <span className="border-white border-2 text-3xl rounded-full px-4 m-2 text-center font-bold">
+              2
+            </span>
             <h1 className="text-xl font-bold">
               Upload your Image that you want to search
             </h1>
           </div>
-          <div className="flex justify-center items-center">
-            <svg
-              className="w-12 h-12 text-black"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </div>
-          <div className="w-[15rem] p-4 bg-[--trinary] rounded-md">
+          <div className="flex flex-col w-[17rem] p-4 bg-[--trinary] rounded-md items-center">
+            <span className="border-white border-2 text-3xl rounded-full px-4 m-2 text-center font-bold">
+              3
+            </span>
             <h1 className="text-xl font-bold">Choose How you serach</h1>
           </div>
-          <div className="flex justify-center items-center">
-            <svg
-              className="w-12 h-12 text-black"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </div>
-          <div className="w-[15rem] p-4 bg-[--trinary] rounded-md">
+          <div className="flex flex-col w-[17rem] p-4 bg-[--trinary] rounded-md items-center">
+            <span className="border-white border-2 text-3xl rounded-full px-4 m-2 text-center font-bold">
+              4
+            </span>
             <h1 className="text-xl font-bold">Search Your Image</h1>
           </div>
         </div>
